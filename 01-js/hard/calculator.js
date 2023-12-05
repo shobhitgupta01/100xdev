66e620da8bc157a98f9e8a8e3e88e0279b0f88b7
Shobhit Gupta <shobhit.gupta1995@gmail.com>
2023-12-05 18:03:01 +0530

    completed the calculator problem
    completed the calculator problem
    All test cases passed

--- 01-js/hard/calculator.js
+++ 01-js/hard/calculator.js
@@ -36,9 +36,9 @@ class Calculator {
   }
 
   divide(val){
-    // if(val === 0){
-    //   throw "cannot divide by zero";
-    // }
+    if(val == 0){
+      throw new Error();
+    }
     this.result /= val;
   }
 
@@ -51,124 +51,30 @@ class Calculator {
   }
 
   calculate(expression){
-    const regExp = /[a-zA-Z]/g;
-    if (regExp.test(expression)){
-      throw "invalid expression"
-    }
-    res = calculate_infix(expression);
+    let res = evalExpression(expression);
     this.result = res;
   }
 
 }
 
-function prec(op){
-  if (op == '^'){
-      return 3;
-  }
-  else if(op == '*' || op=='/'){
-      return 2;
-  }
-  else if(op =='+' || op=='-'){
-      return 1;
-  }
-  else{
-      return -1;
-  }
+function evalExpression(expression){
+    // check validity using regex
+    const validExpression = /^[\d+\-*/().\s]+$/;
+
+    //check if expression is valid
+    if(!validExpression.test(expression)){
+      throw new Error();
+    }
+
+    // removing all spaces
+    const cleanedExpression = expression.replace(/\s+/g, '');
+    const result = eval(cleanedExpression);
+    if(result === Infinity){
+      throw new Error();
+    }
+    return result
+    
 }
 
-function infix_to_postfix(infix){
-  // removing whitespaces
-  infix = infix.replace(/ /g,'');
-  let stack = [];
-  let postfix = '';
-  for(i=0;i<infix.length;i++){
-      char = infix[i];
-
-      // if operand, add to postfix
-      if(char >='0' && char <='9'){
-          postfix+=char;
-      }
-      // if ( , add to stack
-      else if(char == '('){
-          stack.push(char);
-      }
-      // if ). pop from stack till ) is found and then pop it too
-      else if(char==')'){
-          while( stack.length > 0 && stack[stack.length - 1] !='('){
-              postfix+=stack[stack.length - 1];
-              stack.pop();
-          }
-          stack.pop(); // removing the '('
-      }
-      // if operator and prec(chr) <= prec(stack[top]), pop from stack and add to postfix
-      else{
-          while(stack.length > 0 && prec(char) <= prec(stack[stack.length -1])){
-              postfix+=stack[stack.length-1];
-              stack.pop();
-          }
-          stack.push(char);
-      }
-  }
-
-  // emptying the stack into the expression
-  while(stack.length>0){
-      postfix+=stack[stack.length-1];
-      stack.pop();
-  }
-  return postfix;
-}
-
-// infix = "a+b*(c^d-e)^(f+g*h)-i";
-// console.log(infix_to_postfix(infix));
-
-
-// need a function to evaluate postfix notation
-function evaluate_postfix(postfix){
-  // removing whitespaces
-  postfix = postfix.replace(/ /g,'');
-  stack = []
-  operators = ['+','-','*','/','^'];
-  for(i=0; i < postfix.length; i++){
-      char = postfix[i];
-      if( operators.includes(char)){
-          op2 = parseFloat(stack.pop());
-          op1 = parseFloat(stack.pop());
-          res = calculate_op(op1, op2, char);
-          stack.push(res);
-      }
-      else{
-          stack.push(parseFloat(char));
-      }
-  }
-  return stack[stack.length - 1];
-
-}
-
-function calculate_op(op1, op2, operator){
-  if( operator == '+'){
-      return op1 + op2;
-  }
-  else if(operator == '-'){
-      return op1 - op2;
-  }
-  else if(operator == '*'){
-      return op1*op2;
-  }
-  else if(operator == '/'){
-      return op1/op2;
-  }
-  else if(operator == '^'){
-      return op1 ^ op2;
-  }
-}
-
-function calculate_infix(infix){
-  postfix = infix_to_postfix(infix);
-  res = evaluate_postfix(postfix);
-  return res
-}
-
-// infix = '1 + (3/2)'
-// console.log(calculate_infix(infix));
 
 module.exports = Calculator;
